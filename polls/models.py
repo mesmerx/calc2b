4559aaf72498362f9608cfc22cbe351745da4271
mesmerx <mesmer@fisica.if.uff.br>
2016-05-17 00:25:41 -0300

    muitas coisas

--- polls/models.py
+++ polls/models.py
@@ -4,10 +4,8 @@ import datetime
 from django.utils import timezone
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Paginas(models.Model):
+    pagina = models.CharField(max_length=200)
+    categoria = models.CharField(max_length=200)
     def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pagina
--- polls/views.py
+++ polls/views.py
@@ -2,22 +2,13 @@ from django.http import HttpResponse
 
 from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+from .models import Paginas
 
 from django.http import Http404
     
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/inicio.html')
     
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+def gnuplot(request):
+    return render(request, 'polls/gnuplot.html')
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
