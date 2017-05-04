431ab29c2f6a92275eb83c86a215ec71d1b02b2c
mesmer <mesmer@fisica.if.uff.br>
2017-05-04 12:21:39 -0300

    add sagemathtxt

--- polls/templatetags/htmltags.py
+++ polls/templatetags/htmltags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.conf import settings
 import os
+import re
 register = template.Library()
 from django.contrib.sites.shortcuts import get_current_site
 def lower(value): # Only one argument.
@@ -12,6 +13,8 @@ def lower(value): # Only one argument.
 def html(value):
     new_value=value.replace("[intro]","<div style='margin-top:60px;'>")
     new_value=new_value.replace("[/intro]","</div>")
+    matchl=re.findall(r'.*link="(.*?)".*](.*)\[',value)
+    new_value=re.sub('\[*link=".*\[\\\link]','<a href="{0}">{1}</a>'.format(matchl[0][0],matchl[0][1]),value)
     new_value=new_value.replace("[titulo]","<h3>")
     new_value=new_value.replace("[/titulo]","</h3>")
     new_value=new_value.replace("[texto]","<div><pre><p>")
